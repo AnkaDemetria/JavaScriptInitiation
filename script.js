041200227cecfff47691991d261dffc88d27fc29
AnkaDemetria <116574892+AnkaDemetria@users.noreply.github.com>
2023-02-14 09:24:20 +0100

    commit1

---
+++ script.js
@@ -0,0 +1,22 @@
+// CREER 4 CONSTANTES PRENOM NOM AGE ET PHRASE
+const prenom="Sailor";
+const nom="Moon";
+const age=25;
+const phrase="Bonjour, je m'appelle  " +prenom+ " "  +nom +"  et j'ai " +age +" ans";
+// alert(phrase);
+
+// AFFICHER LA DATE DU JOUR
+var now=new Date();
+console.log(now);
+
+// CREER UN TABLEAU 
+// const monTableau = ["toto","tutu","tati"];
+// monTableau.pop;
+// console.table(monTableau);
+
+// CREER UN TABLEAU ASSOCIATIF SANS EN FAIRE UN OBJET
+const tableau = [];
+tableau["papa"]="toto";
+tableau["maman"]="tati";
+tableau["enfant"]="titi";
+console.table(tableau);
